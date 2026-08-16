281c56cc96e996b1110a6cc9cb0e67945ebdf7b3
Amey Khoje <ameykhoje@gmail.com>
2026-08-16 03:59:10 -0400

    pylint changes

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+""" This is server file for Emotion Detection Project """
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +7,15 @@ app = Flask("Emotion Detection")
 
 @app.route("/")
 def render_template_html():
+    """ Route Method ro render HTML template at Index route """
     return render_template('index.html')
 
 @app.route("/emotionDetector", methods=["GET"])
 def emotion_detector_handler():
+    """ Route Method to analyse emotions """
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
-    if (result['dominant_emotion'] == None):
+    if result['dominant_emotion'] is None:
         return "Invalid text! Please try again."
     return result, 200
 
