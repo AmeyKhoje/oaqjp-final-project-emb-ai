0bc38c4b59bb90820ae7e467b155e655a999d923
Amey Khoje <ameykhoje@gmail.com>
2026-08-16 03:44:01 -0400

    Added server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/")
+def render_template_html():
+    return render_template('index.html')
+
+@app.route("/emotionDetector", methods=["GET"])
+def emotion_detector_handler():
+    text_to_analyze = request.args.get("textToAnalyze")
+    result = emotion_detector(text_to_analyze)
+    return result, 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
